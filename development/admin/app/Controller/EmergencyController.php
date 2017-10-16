48b8cd1c59b54f3952248fe098e98e8d53cb8f8d
torii <staff@pull-net.jp>
2017-10-16 20:14:05 +0900

    10/16 忘れてるファイルを追加...

---
+++ development/admin/app/Controller/EmergencyController.php
@@ -0,0 +1,160 @@
+<?php
+/* ------------------------------------------------------------------- 	*/
+/*	PULL-NET.Inc							*/
+/*	Masato Nakatsuji						*/
+/*	2016/12/06							*/
+/*									*/
+/*	Collabos_ver2.0(管理)						*/
+/*	https://admin.collabos.jp					*/
+/*									*/
+/*	地区管理画面					*/
+/*	EmergencyController.php					*/
+/* ------------------------------------------------------------------- 	*/
+App::uses('AppController', 'Controller');
+
+class EmergencyController extends AppController{
+
+	public $uses=array(
+		"Contents",
+		"Emergencies",
+		"Category",
+		"District",
+	);
+
+	public $components=array(
+		"Db",
+		"Loadbasic",
+		"Csv",
+	);
+
+	public function beforeFilter(){
+		parent::beforeFilter();
+	}
+	//★ページカテゴリー一覧
+	public function index($page=1){
+		$limit=30;
+		$this->set("page",$page);
+		$this->set("limit",$limit);
+		
+		
+		//地区情報をset
+		$district_list=$this->Category->find("list",array(
+			"fields"=>array("id","name"),
+			"conditions"=>array(
+				"Category.type_mode"=>"1",						
+			),
+		));
+		$this->set("district_list",$district_list);		
+		
+		//コンテンツ情報をset
+		$result=$this->Emergencies->find("all",array(
+			"limit"=>$limit,
+			"page"=>$page,
+		));
+		$this->set("result",$result);
+
+		$totalcount=$result=$this->Emergencies->find("count");
+		$totalpage=ceil($totalcount/$limit);
+		$this->set("totalcount",$totalcount);
+		$this->set("totalpage",$totalpage);
+
+		$this->set("wwwurl",$this->Loadbasic->load("wwwurl"));
+	}
+	//★ページカテゴリー登録・編集
+	public function edit($id=null){
+		
+		//カテゴリー情報をset
+		$category_list=$this->Category->find("list",array(
+			"fields"=>array("id","name"),
+			"conditions"=>array(
+				"Category.type_mode"=>"0",						
+			),
+		));
+		$this->set("category_list",$category_list);
+		
+
+		if($this->request->data){
+			$post=$this->request->data;
+
+			$this->Emergencies->set($post);
+			if($this->Emergencies->validates()){
+
+				//詳細情報をJSON化して登録・更新
+				$caption=json_encode(array(
+					"ttl1"=>$post["Contents"]["ttl1"],
+					"ttl2"=>$post["Contents"]["ttl2"],
+					"ttl3"=>$post["Contents"]["ttl3"],
+					"ttl4"=>$post["Contents"]["ttl4"],
+					"ttl5"=>$post["Contents"]["ttl5"],
+					"text1"=>$post["Contents"]["text1"],
+					"text2"=>$post["Contents"]["text2"],
+					"text3"=>$post["Contents"]["text3"],
+					"text4"=>$post["Contents"]["text4"],
+					"text5"=>$post["Contents"]["text5"],
+				),JSON_UNESCAPED_UNICODE);
+				
+				$post["Contents"]["caption"]=$caption;
+
+				//店舗情報をJSON化して登録・更新
+				$shop_info=json_encode(array(
+					"postnumber"=>$post["Contents"]["postnumber"],
+					"address"=>$post["Contents"]["address"],
+					"tel"=>$post["Contents"]["tel"],
+					"shop_text"=>$post["Contents"]["shop_text"],	
+				),JSON_UNESCAPED_UNICODE);
+				$post["Contents"]["shop_info"]=$shop_info;
+				
+				//画像をadditem登録・更新
+				$post["Additems"]["content_id"]=$post["Contents"]["id"];
+
+				$this->Contents->save($post,false);
+
+				$this->Session->write("alert","コンテンツを１件設定しました。");
+				$this->redirect(array("controller"=>"contents","action"=>"index"));
+			}
+		}
+		else{
+			if($id){
+
+				$post=$this->Emergencies->find("first",array(
+					"conditions"=>array(
+						"Contents.id"=>$id,
+					),
+				));
+	
+				$caption=json_decode(@$post["Contents"]["caption"],true);
+				if(@$caption){
+					$post["Contents"]=array_merge($post["Contents"],@$caption);
+				}
+				$shop_info=json_decode(@$post["Contents"]["shop_info"],true);
+				if(@$shop_info){
+					$post["Contents"]=array_merge($post["Contents"],@$shop_info);
+				}				
+
+				$this->request->data=$post;
+			}
+		}
+	}
+
+	//★地区・削除
+	public function delete($id){
+		
+		$this->autoRender=false;
+		
+		//idでテーブルデータ取得
+		$result=$this->Emergencies->find("first",array(
+			'conditions' => array(
+				'Contents.id' => $id,
+			)
+		));
+		//idでテーブルデータ削除
+		$this->Contents->delete($id);
+		
+		//テキスト表示とリダイレクト
+		$this->Session->write("alert", "地区を削除いたしました。");
+		$this->redirect(array("controller"=>"contents","action"=>"index"));
+
+	}	
+
+	
+}
---
+++ development/admin/app/View/Emergency/index.ctp
@@ -0,0 +1,62 @@
+<div class="bread"><?php echo $this->Html->link("管理TOP","/"); ?>　＞　コンテンツ一覧</div>
+<h1>コンテンツ一覧</h1>
+<?php
+if(isset($alert))
+{
+?>
+	<div class="alert-message"><?php echo $alert; ?></div>
+<?php
+}
+?>
+<div class="right mb10">
+	<?php echo $this->Html->link("コンテンツ新規登録",array("controller"=>"contents","action"=>"edit"),array("class"=>"buttons")); ?>
+</div>
+<p class="h3">全<?php echo $totalcount; ?>件</p>
+<p class="mb10">(<?php echo $page; ?>P/<?php echo $totalpage; ?>P)</p>
+
+	<table cellspacing="0" cellpadding="0" class="list mb20">
+	<tr>
+		<th class="micro">No</th>
+		<th class="minishort">登録日</th>
+		<th>コンテンツタイトル</th>
+		<th class="minishort">カテゴリー</th>
+		<th class="minishort">地区</th>
+		<th class="minishort"></th>
+	</tr>
+	<?php
+	$count=$limit*($page-1);
+	foreach($result as $r_){
+		$count++;
+	?>
+	<tr>
+		<td class="center"><?php echo $count; ?></td>
+		<td><?php echo date("Y.m.d H:i",strtotime($r_["Contents"]["createdate"])); ?></td>
+		<td><?php echo h($r_["Contents"]["title"]); ?></td>
+		<td><?php echo $category_list[ $r_["Contents"]["category_id"] ]; ?></td>
+		<td><?php echo $district_list[ $r_["Contents"]["district_id"] ]; ?></td>
+
+		<td>
+			<?php echo $this->Html->link("編集",array("controller"=>"contents","action"=>"edit",$r_["Contents"]["id"]),array("class"=>"buttons")); ?>
+
+			<label for="delete_<?php echo $count; ?>" class="buttons del">削除</label>
+			<div id="popup">
+				<input type="checkbox" id="delete_<?php echo $count; ?>" class="checks">
+				<label></label>
+				<div class="basejavar"></div>
+				<div class="window short">
+					<div class="bs">
+						<h2 class="mb30">「<?php echo $r_["Contents"]["title"]; ?>」のページカテゴリーを削除します</h2>
+						<div class="center">
+							<label for="delete_<?php echo $count; ?>" class="buttons">キャンセル</label>
+							<?php echo $this->Html->link("削除する",array("controller"=>"contents","action"=>"delete",$r_["Contents"]["id"]),array("class"=>"buttons del")); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</td>
+	</tr>
+	<?php
+	}
+	?>
+	</table>
